0407bf1406daeed4263fee5ff01d62f65d00a9ac
Mark <SrtDemon.com>
2022-01-10 21:12:17 +0300

    Homework 16.4

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,23 +4,28 @@
 #include <string>
 
 
-const int N = 100;
-
-int Vivod(int a) 
-{
-	
-	for (size_t i = a; i <= N; i += 2)
-	{
-		std::cout << i << '\n';
-	}
-	return 0;
-}
+const int N = 4;
 
 int main()
 {
-	int type;
-	std::cout << "Enter 0 for even number and 1 for odd number" << '\n';
-	std::cin >> type;
-	Vivod(type);
+	int sum = 0;
+	int F = 10%N; // остаток от деления(сегодня 10е число)
+	int mass[N][N];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			mass[i][j] = i + j;
+			std::cout << mass[i][j] << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	for(int h = 0; h < N; h++)
+	{
+		sum += mass[F - 1][h];
+	}
+
+	std::cout << "summ of values = " << sum;
+	
 	return 0;
 }
