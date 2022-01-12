9528fa8659c4dd06e76ae5b6f7d506ac02d637cf
Mark <SrtDemon.com>
2022-01-12 22:04:18 +0300

    HomeWork 17.5

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
-#include "Helpers.h"
-#include <iomanip>
-#include <string>
+#include <math.h>
 
 
-const int N = 4;
+class Vector
+{
+public:
+	Vector() : x(1), y(4), z(2)
+	{}
+	void VivodCoord()
+	{
+		std::cout << "x = " << x << ",y = " << y << ",z = " << z << '\n';
+	}
+	double Module(double x, double y,double z)
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+private:
+	double x, y, z;
+};
 
 int main()
 {
-	int sum = 0;
-	int F = 10%N; // остаток от деления(сегодня 10е число)
-	int mass[N][N];
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			mass[i][j] = i + j;
-			std::cout << mass[i][j] << ' ';
-		}
-		std::cout << '\n';
-	}
-
-	for(int h = 0; h < N; h++)
-	{
-		sum += mass[F - 1][h];
-	}
-
-	std::cout << "summ of values = " << sum;
-	
+	double xC, yC, zC;
+	std::cout << "Input custom x, y and z";
+	std::cin >> xC >> yC >> zC;
+	Vector first;
+	first.VivodCoord();
+	std::cout << "Module of custom vector = " << first.Module(xC, yC, zC);
 	return 0;
 }
